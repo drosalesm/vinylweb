5b0d778fdf6dc7df4549589584b5824ae24bcf11
David Rosales Mejia <drosalesm204@gmail.com>
2022-10-04 22:10:09 -0600

    new realize

--- vinilweb/wsgi.py
+++ vinilweb/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vinilweb.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vinilweb.settings.local')
 
 application = get_wsgi_application()
