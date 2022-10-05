f1caed43ec194b337754e7cc193b7fc7dbe1d06e
David Rosales Mejia <drosalesm204@gmail.com>
2022-10-05 17:12:39 -0600

    new realize

--- vinilweb/settings/prod.py
+++ vinilweb/settings/prod.py
@@ -5,16 +5,16 @@ from .base import *
 ALLOWED_HOSTS = ['vinilwebhn.herokuapp.com']
 
 #ALLOWED_HOSTS = ['*']
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'da435ej10i352j',
-        'USER': 'lkuvnmjabcrixk',
-        'PASSWORD': '15683ab85c93f7c61aba3180f79f24e94d49fb0a22f7fb23e6d073b010227838',
-        'HOST': 'ec2-52-20-166-21.compute-1.amazonaws.com',
-        'PORT': '5432',
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'da435ej10i352j',
+#        'USER': 'lkuvnmjabcrixk',
+#        'PASSWORD': '15683ab85c93f7c61aba3180f79f24e94d49fb0a22f7fb23e6d073b010227838',
+#        'HOST': 'ec2-52-20-166-21.compute-1.amazonaws.com',
+#        'PORT': '5432',
+#    }
+#}
 
 
 #DATABASES = {
