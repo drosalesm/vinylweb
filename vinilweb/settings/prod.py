76b3262e69e8a6cd5db454d90f48029e0bf9a401
David Rosales Mejia <drosalesm204@gmail.com>
2022-10-05 22:26:09 -0600

    new realize

--- vinilweb/settings/prod.py
+++ vinilweb/settings/prod.py
@@ -4,29 +4,29 @@ from .base import *
 # Database
 ALLOWED_HOSTS = ['vinilwebhn.herokuapp.com']
 
-#ALLOWED_HOSTS = ['*']
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': 'da435ej10i352j',
-#        'USER': 'lkuvnmjabcrixk',
-#        'PASSWORD': '15683ab85c93f7c61aba3180f79f24e94d49fb0a22f7fb23e6d073b010227838',
-#        'HOST': 'ec2-52-20-166-21.compute-1.amazonaws.com',
-#        'PORT': '5432',
-#    }
-#}
-
-
+ALLOWED_HOSTS = ['*']
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'vinylwebhndb',
-        'USER': 'jdmejia',
-        'PASSWORD': '123david',
-        'HOST': 'database-1.cqxiuo4vikrc.us-east-1.rds.amazonaws.com',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'da435ej10i352j',
+        'USER': 'lkuvnmjabcrixk',
+        'PASSWORD': '15683ab85c93f7c61aba3180f79f24e94d49fb0a22f7fb23e6d073b010227838',
+        'HOST': 'ec2-52-20-166-21.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
+
+
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': 'vinylwebhndb',
+#        'USER': 'jdmejia',
+#        'PASSWORD': '123david',
+#        'HOST': 'database-1.cqxiuo4vikrc.us-east-1.rds.amazonaws.com',
+#        'PORT': '5432',
+#    }
+#}
 
 
 
