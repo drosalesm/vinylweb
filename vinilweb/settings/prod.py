ee6f0738d09d0930300d3044e772153d0f0bcbaf
David Rosales Mejia <drosalesm204@gmail.com>
2022-10-06 09:59:52 -0600

    new realize

--- vinilweb/settings/prod.py
+++ vinilweb/settings/prod.py
@@ -11,7 +11,17 @@ DATABASES = {
     )
 }
 
-
+#ALLOWED_HOSTS = ['*']
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'da435ej10i352j',
+#        'USER': 'lkuvnmjabcrixk',
+#        'PASSWORD': '15683ab85c93f7c61aba3180f79f24e94d49fb0a22f7fb23e6d073b010227838',
+#        'HOST': 'ec2-52-20-166-21.compute-1.amazonaws.com',
+#        'PORT': '5432',
+#    }
+#}
 
 
 #DATABASES = {
